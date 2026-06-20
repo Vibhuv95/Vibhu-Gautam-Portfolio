e323bfd79b1634a32a3f3fc1d6471d8e860e4c07
vIbHugAUtaMcOdE <vibhugautam8081371284@gmail.com>
2026-06-20 14:41:39 +0530

    Portfolio First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Data Analyst", "Power BI Developer", "SQL Enthusiast", "Data Science Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
