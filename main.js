16d7775bb95d3b511d3df9eb65c599fe44508f3e
vIbHugAUtaMcOdE <vibhugautam8081371284@gmail.com>
2026-07-08 04:04:14 +0530

    Add Emailjs for Contact Form

--- main.js
+++ main.js
@@ -5,3 +5,46 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+emailjs.init("kpi7E0rxWKlNUjkxe");
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const button = form.querySelector(".send");
+
+    button.innerHTML = "Sending...";
+    button.disabled = true;
+
+    emailjs.sendForm(
+        "service_xcacxie",
+        "template_yiqodfk",
+        this
+    )
+
+        .then(() => {
+
+            alert("✅ Message Sent Successfully!");
+
+            form.reset();
+
+            button.innerHTML = "Send Message";
+            button.disabled = false;
+
+        })
+
+        .catch((error) => {
+
+            console.log("EmailJS Error:", error);
+
+            alert(JSON.stringify(error));
+
+            button.innerHTML = "Send Message";
+            button.disabled = false;
+
+        });
+
+});
